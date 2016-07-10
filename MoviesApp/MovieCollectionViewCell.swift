16f38c4c5543a82502253445519cb220f4c6dec3
tcltien <atien.lh@gmail.com>
2016-07-11 02:11:00 +0700

    Commit Readme

---
+++ MoviesApp/MovieCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionViewCell.swift
+//  MoviesApp
+//
+//  Created by Le Huynh Anh Tien on 7/11/16.
+//  Copyright © 2016 Tien Le. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+}
